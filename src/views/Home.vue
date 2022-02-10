2fcfafc1c7d5807cba84076ca5b40478b3a4a7d5
Meghan Scherzer <meghan.scherzer@gmail.com>
2022-02-09 18:24:48 -0600

    adds all CRUD methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,9 @@ export default {
     return {
       message: "Welcome to the Movvie App!",
       movies: [],
-      newMovie: [],
+      newMovieParams: {},
+      currentMovie: {},
+      editMovieParams: {},
     };
   },
   created: function () {
@@ -20,30 +22,41 @@ export default {
       });
     },
     createMovie: function () {
-      var params = {
-        title: "Yes Man",
-        year: 2012,
-        plot: "A man says yes to everything.",
-        director: "Stephan Speilberg",
-        english: true,
-      };
       axios
-        .post("http://localhost:3000/movies", params)
+        .post("http://localhost:3000/movies", this.newMovieParams)
         .then((response) => {
-          console.log("Success!", response.data), this.movies.push(response.data);
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+          this.newMovieParams = {};
         })
         .catch((error) => {
-          console.log(error.response);
+          console.log(error.response.data.errors);
         });
     },
-    showMovie: function () {
-      var params = {
-        id: 2,
-      };
-      axios.get("http://localhost:3000/movies", params).then((response) => {
-        console.log(response.data);
-        this.movies = response.data;
-      });
+    showMovie: function (movie) {
+      console.log(movie);
+      this.currentMovie = movie;
+      this.editMovieParams = movie;
+      document.querySelector("#movie-details").showModal();
+    },
+    updateMovie: function (movie) {
+      axios
+        .patch(`http://localhost:3000/movies/${movie.id}`, movie)
+        .then((response) => {
+          console.log("Yippee!", response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyMovie: function (movie) {
+      if (confirm("Do you reallllllyyyyy want to do this???")) {
+        axios.delete(`http://localhost:3000/movies/${movie.id}`).then((response) => {
+          console.log("You da wo-man!", response.data);
+          var index = this.movies.indexOf(movie);
+          this.movies.splice(index, 1);
+        });
+      }
     },
   },
 };
@@ -52,13 +65,62 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div><input type="text" v-model="newMovie" /></div>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieParams.title" />
+    </div>
+    <div>
+      Year:
+      <input type="text" v-model="newMovieParams.year" />
+    </div>
+    <div>
+      Plot:
+      <input type="text" v-model="newMovieParams.plot" />
+    </div>
+    <div>
+      Director:
+      <input type="text" v-model="newMovieParams.director" />
+    </div>
+    <div>
+      English:
+      <input type="text" v-model="newMovieParams.english" />
+    </div>
     <button v-on:click="createMovie()">Create Movie</button>
+    <br />
     <h2>All Movies</h2>
-    <div v-for="movie in movies" v-bind:key="movie.id">
+    <div v-for="movie in movies" :key="movie.id">
       <h3>Title: {{ movie.title }}</h3>
-      <p>Director: {{ movie.director }}</p>
-      <button v-on:click="showMovies()">More Info</button>
+      <button v-on:click="showMovie()">More Info</button>
+    </div>
+    <div>
+      <dialog id="movie-details">
+        <form method="dialog">
+          <h1>Movie Info</h1>
+          <p>
+            Title:
+            <input type="text" v-model="editMovieParams.title" />
+          </p>
+          <p>
+            Year:
+            <input type="text" v-model="editMovieParams.year" />
+          </p>
+          <p>
+            Director:
+            <input type="text" v-model="editMovieParams.director" />
+          </p>
+          <p>
+            Plot:
+            <input type="text" v-model="editMovieParams.plot" />
+          </p>
+          <p>
+            English:
+            <input type="text" v-model="editMovieParams.english" />
+          </p>
+          <button v-on:click="updateMovie(currentMovie)">Update</button>
+          <button v-on:click="destroyMovie(currentMovie)">Destroy?!</button>
+          <button>Close</button>
+        </form>
+      </dialog>
     </div>
   </div>
 </template>
